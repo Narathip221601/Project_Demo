4d205cc6f5f4b530e30a44b33dc70b02dea1602b
knattapon <nattapontarn@gmail.com>
2020-12-16 15:26:04 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import Dog from "./assets/pug.jpg";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
+export default function App() {
+  // const onFinish = values => {
+  //   console.log('Received values of form: ', values);
+  // };
+
+  const onFinish = (values) => {
+    Axios.post("https://covid19.th-stat.com/api/open/today", {
+      user_name: "superadmin@ttt.com",
+      password: "RTAF_Telecom@1234",
+    })
+      .then(({ data }) => {
+        console.log(data);
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // const login=()=>{
+  //   // axiost.post('http://47.241.24.240:8081/token', {
+  //   //   "user_name": "superadmin@ttt.com",
+  //   //   "password": "RTAF_Telecom@1234",
+  //   //   // "user_name": "superadmin@ttt.com",
+  //   //   // "password": "RTAF_Telecom@1234",
+  //   // }).then(({ data }) => {
+  //   //   console.log(data);
+
+  //   // }).catch((error) => {
+  //   //   console.log(error)
+  //   // })
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form
+      style={{ textAlign: "center" }}
+      name="normal_login"
+      className="child"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      <div className="container">
+        <img src={Dog} width="100%" />
+      </div>
+      <Form.Item
+        className="container2"
+        name="username"
+        rules={[{ required: true, message: "กรุณาใส่ยูสเซอร์" }]}
+      >
+        <Input prefix={<UserOutlined />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: "กรุณาใส่รหัสผ่าน" }]}
+      >
+        <Input
+          prefix={<LockOutlined />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
-
-export default App;
